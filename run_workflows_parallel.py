24e8481d1d5be2b36a3daa4ad410ec9ed82d9b19
Kevin Liu <kevinliu.bmb@gmail.com>
2023-07-10 12:38:31 -0400

    Fixed multiprocessing protection issue

--- run_workflows_parallel.py
+++ run_workflows_parallel.py
@@ -9,32 +9,55 @@ model_path = "example_data/models"
 mbx_path = "example_data/metabolomics_data.csv"
 output_path = "example_outputs"
 
-# Search through the model directory and find all the JSON and MATLAB files
-model_files = [f for f in os.listdir(model_path) if f.endswith(".json")]
-model_files_mat = [f for f in os.listdir(model_path) if f.endswith(".mat")]
 
-# If the number of JSON files is less than the number of MATLAB files, convert
-# all models to JSON format
-if len(model_files) < len(model_files_mat):
-    for model in model_files_mat:
-        convert_model_format(f"{model_path}/{model}", model_path)
+def main(model_path, mbx_path, output_path):
+    # Search through the model directory and find all the JSON and MATLAB files
+    model_files = [f for f in os.listdir(model_path) if f.endswith(".json")]
+    model_files_mat = [f for f in os.listdir(model_path) if f.endswith(".mat")]
 
-# Search through the model directory and find all the JSON files
-model_files = [f for f in os.listdir(model_path) if f.endswith(".json")]
+    # If the number of JSON files is less than the number of MATLAB files, convert
+    # all models to JSON format
+    if len(model_files) < len(model_files_mat):
+        for model in model_files_mat:
+            convert_model_format(f"{model_path}/{model}", model_path)
 
-for model_file in model_files:
-    # Arguments for the functions
-    args_optimize_model = [f"{model_path}/{model_file}", output_path, False, True, False, True]
-    args_optimize_model_mbx = [f"{model_path}/{model_file}", mbx_path, output_path, True, False, False, True]
+    # Search through the model directory and find all the JSON files
+    model_files = [f for f in os.listdir(model_path) if f.endswith(".json")]
 
-    # Initialize ProcessPoolExecutor
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        # Submit the functions to the pool of workers
-        future_optimize_model = executor.submit(optimize_model, *args_optimize_model)
-        future_optimize_model_mbx = executor.submit(
-            optimize_model_mbx, *args_optimize_model_mbx
-        )
+    for model_file in model_files:
+        # Arguments for the functions
+        args_optimize_model = [
+            f"{model_path}/{model_file}",
+            output_path,
+            False,
+            True,
+            False,
+            True,
+        ]
+        args_optimize_model_mbx = [
+            f"{model_path}/{model_file}",
+            mbx_path,
+            output_path,
+            True,
+            False,
+            False,
+            True,
+        ]
 
-        # Retrieve the results
-        future_optimize_model.result()
-        future_optimize_model_mbx.result()
+        # Initialize ProcessPoolExecutor
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            # Submit the functions to the pool of workers
+            future_optimize_model = executor.submit(
+                optimize_model, *args_optimize_model
+            )
+            future_optimize_model_mbx = executor.submit(
+                optimize_model_mbx, *args_optimize_model_mbx
+            )
+
+            # Retrieve the results
+            future_optimize_model.result()
+            future_optimize_model_mbx.result()
+
+
+if __name__ == "__main__":
+    main(model_path, mbx_path, output_path)
