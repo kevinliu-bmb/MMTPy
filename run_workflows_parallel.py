871080a44085711602bcc456d90bcfca41f08c6c
Kevin Liu <kevinliu.bmb@gmail.com>
2023-07-12 14:42:40 -0400

    Added more example data

--- run_workflows_parallel.py
+++ run_workflows_parallel.py
@@ -6,7 +6,7 @@ from mmtpy_utils import convert_model_format
 
 # Define paths
 model_path = "example_data/models"
-mbx_path = "example_data/metabolomics_data.csv"
+mbx_path = "example_data/metabolomics_data_assert_non_large_value.csv"
 output_path = "example_outputs"
 
 
