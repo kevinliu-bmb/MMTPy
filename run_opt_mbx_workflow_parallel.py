efb4abbd140da1f60a4ac2f2e894bc30c7229fb7
Kevin Liu <kevinliu.bmb@gmail.com>
2023-07-12 15:02:13 -0400

    Added function to run only MBX opt in parallel

---
+++ run_opt_mbx_workflow_parallel.py
@@ -0,0 +1,55 @@
+import concurrent.futures
+import os
+
+from mmtpy_opt_workflows import optimize_model_mbx
+from mmtpy_utils import convert_model_format
+
+# Define paths
+model_path = "example_data/models"
+mbx_path = "example_data/metabolomics_data_assert_non_large_value.csv"
+output_path = "example_outputs"
+
+
+def main(model_path, mbx_path, output_path):
+    # Search through the model directory and find all the JSON and MATLAB files
+    model_files = [f for f in os.listdir(model_path) if f.endswith(".json")]
+    model_files_mat = [f for f in os.listdir(model_path) if f.endswith(".mat")]
+
+    # If the number of JSON files is less than the number of MATLAB files, convert
+    # all models to JSON format
+    if len(model_files) < len(model_files_mat):
+        for model in model_files_mat:
+            convert_model_format(f"{model_path}/{model}", model_path)
+
+    # Search through the model directory and find all the JSON files
+    model_files = [f for f in os.listdir(model_path) if f.endswith(".json")]
+
+    # Initialize ProcessPoolExecutor
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # Submit the function to the pool of workers for each model file
+        futures = []
+        for model_file in model_files:
+            # Arguments for the function
+            args_optimize_model_mbx = [
+                f"{model_path}/{model_file}",
+                mbx_path,
+                output_path,
+                True,
+                False,
+                False,
+                True,
+            ]
+
+            # Submit the function to the pool of workers
+            future_optimize_model_mbx = executor.submit(
+                optimize_model_mbx, *args_optimize_model_mbx
+            )
+            futures.append(future_optimize_model_mbx)
+
+        # Retrieve the results
+        for future in futures:
+            future.result()
+
+
+if __name__ == "__main__":
+    main(model_path, mbx_path, output_path)
