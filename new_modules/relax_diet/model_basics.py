11d79686c110da3f48ae536c57ffe5654a631f55
Kevin Liu <kevinliu.bmb@gmail.com>
2023-09-13 11:09:39 -0400

    Format improvements

--- new_modules/relax_diet/model_basics.py
+++ new_modules/relax_diet/model_basics.py
@@ -1,9 +1,17 @@
-def model_info(model_obj, biomass_opt:bool = True):
+from csv import reader
+from csv import DictReader
+from cobra.core import Group
+from optlang.gurobi_interface import Constraint
+from cobra.io import load_matlab_model
+from cobra.io import load_json_model
+
+def model_info(model_obj, biomass_opt : bool = True) -> None:
     if biomass_opt:
         with model_obj:
             bmass_val = str(model_obj.slim_optimize())
     else:
-        bmass_val = 'N/A'
+        bmass_val = "N/A"
+
     print(f'''
 -----------
 Name: {model_obj.name}
@@ -21,23 +29,16 @@ Biomass Flux: {bmass_val}
 ''')
 
 def get_species(abun_path) -> list:
-    from csv import DictReader
-
-    with open(abun_path, 'r') as f:
-        read = DictReader(f, delimiter=',')
-
-        species = [row['X'].strip() for row in read]
+    with open(abun_path, "r") as f:
+        read = DictReader(f, delimiter=",")
+        species = [row["X"].strip() for row in read]
 
         return species
 
-def get_diet_info(diet_path):
-    from csv import reader
-
+def get_diet_info(diet_path) -> dict:
     diet_info = dict()
-
-    with open(diet_path, 'r') as f:
-        read = reader(f, delimiter='\t')
-
+    with open(diet_path, "r") as f:
+        read = reader(f, delimiter="\t")
         for row in read:
             diet_info[row[0].strip()] = float(row[1])
 
@@ -46,63 +47,61 @@ def get_diet_info(diet_path):
 def check_constraints(model_obj): #TODO log results
     with model_obj:
         def has_sv():
-            sv = [const for const in model_obj.constraints if 'sv_' in const.name]
+            sv = [const for const in model_obj.constraints if "sv_" in const.name]
 
             if len(sv) > 0 and len(sv) == len(model_obj.metabolites):
                 return True
             
         def has_bmass():
-            bmass = [const for const in model_obj.constraints if 'bmass' in const.name]
+            bmass = [const for const in model_obj.constraints if "bmass" in const.name]
 
             if len(bmass) == 1:
                 return True
-
-
+            
         sv_check = has_sv()
         bmass_check = has_bmass()
 
-        if sv_check: print('SV=0 set')
-        else: print('SV=0 missing')
+        if sv_check: 
+            print("SV=0 set")
+        else: 
+            print("SV=0 missing")
 
-        if bmass_check: print('Biomass constrained')
-        else: print('Missing biomass constraint')
-
-        if sv_check and bmass_check: return True
-        else: return False
+        if bmass_check: print("Biomass constrained")
+        else: 
+            print("Missing biomass constraint")
+        
+        if sv_check and bmass_check: 
+            return True
+        else: 
+            return False
 
 def model_setup(model_path, abun_path, diet_path):
-    ending = model_path.split('.')[-1]
-    name = model_path.split('/')[-1].split('_')[0]
+    ending = model_path.split(".")[-1]
+    name = model_path.split("/")[-1].split("_")[0]
     # print(name)
 
-    if 'json' in ending:
-        from cobra.io import load_json_model
-
+    if "json" in ending:
         model = load_json_model(model_path)
 
-    elif 'mat' in ending:
-        from cobra.io import load_matlab_model
-
+    elif "mat" in ending:
         model = load_matlab_model(model_path)
 
-    else: print('File type currently not supported. Use .json or .mat model')
-
+    else: print("File type currently not supported. Use .json or .mat model")
+    
     if model:
-        from optlang.gurobi_interface import Constraint
-
         model.name = name
 
-        print('Model Loaded')
+        print("Model Loaded")
         model_info(model)
 
         to_add = []
         #SECTION setting biomass constraint
-        biomass = model.reactions.get_by_id('communityBiomass')
-        print('Biomass constrained to 0.4 - 1')
+        biomass = model.reactions.get_by_id("communityBiomass")
+        print("Biomass constrained to 0.4 - 1")
         to_add.append(
-            Constraint(name='bmass_const', expression=biomass.flux_expression, lb=0.4, ub=1)
+            Constraint(name="bmass_const", expression=biomass.flux_expression, lb=0.4, ub=1)
         )
-
+        
         #SECTION setting SV=0
         for met in model.metabolites:
             print(f'({model.metabolites.index(met) + 1} / {len(model.metabolites)})\tNow adding Sv=0 for {met.id}')
@@ -116,16 +115,14 @@ def model_setup(model_path, abun_path, diet_path):
             expr = sum(mulled)
 
             same_flux = Constraint(
-                name='sv_' + met.id,
+                name="sv_" + met.id,
                 expression=expr,
                 lb=0, ub=0
             )
-
+    
             to_add.append(same_flux)
         
         #SECTION add species groups
-        from cobra.core import Group
-
         species = get_species(abun_path)
         groups = []
 
@@ -137,7 +134,7 @@ def model_setup(model_path, abun_path, diet_path):
                 spec_group = Group(
                     id=spec,
                     members=rxns,
-                    kind='collection'
+                    kind="collection"
                 )
 
                 groups.append(spec_group)
@@ -151,11 +148,11 @@ def model_setup(model_path, abun_path, diet_path):
         model_info(model)
 
         #SECTION setting diet #TODO automatic diet selection based on sample location information
-        print('Setting diet')
+        print("Setting diet")
 
         diet = get_diet_info(diet_path)
 
-        diet_rxns = [rxn for rxn in model.reactions if '[d]' in rxn.id]
+        diet_rxns = [rxn for rxn in model.reactions if "[d]" in rxn.id]
 
         for rxn in diet_rxns:
             rxn.lower_bound = 0
@@ -164,7 +161,6 @@ def model_setup(model_path, abun_path, diet_path):
         
         for rxn in rxns:
             rxn.lower_bound = diet[rxn.id]
-
 
         model_info(model)
 
